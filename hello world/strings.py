ea2227c73865be83e30f439ee3d1a979a8c672fe
John Obertubbesing <oberthewhat@gmail.com>
2021-05-28 00:27:25 -0500

    adding python directory

---
+++ hello world/strings.py
@@ -0,0 +1,19 @@
+greeting = "hello"
+name = input("please enter your name ")
+
+print(greeting +" "+ name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+#type shows the variable type
+#print is bascially console.log
+
+age = '2 years'
+
+print(age)
+
+print(type(age))
