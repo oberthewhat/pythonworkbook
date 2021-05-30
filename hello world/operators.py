af598c4f7250a403a645b24be77d29562a4ee802
John Obertubbesing <oberthewhat@gmail.com>
2021-05-30 11:21:50 -0500

    adding more workbook stuff

---
+++ hello world/operators.py
@@ -0,0 +1,16 @@
+a = 12
+b = 3
+
+print(a + b)    # 15
+print(a - b)    # 9
+print(a * b)    # 36
+print(a / b)    # 4.0
+print (a // b)  # 4
+print(a % b)    # 0
+
+print()
+
+print(a + b / 3 - 4 * 12)
+print(a + (b/3) - (4*12))
+
+ 
---
+++ hello world/strings2.py
@@ -0,0 +1,20 @@
+parrot = "Norwegian Blue"
+
+print('using indexs.... def has to be an easier way to do this')
+
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print('using negative indexs')
+
+print(parrot[-11])
+print(parrot[-1])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
