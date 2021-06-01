698b87fa8dab4d82d8bc16347690076577788748
John Obertubbesing <oberthewhat@gmail.com>
2021-06-01 12:06:01 -0500

    practicing with strings

---
+++ hello world/sliceBack.py
@@ -0,0 +1,16 @@
+      #    01234567891234567891234567 
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+backwards = letters[::-1]
+
+print(backwards)
+
+print()
+
+print(letters[16:13:-1])
+
+print(letters[4::-1])
+
+print(letters[:16:-1]) 
+print(letters[-4:])
+print(letters[-1:])
--- hello world/strings2.py
+++ hello world/strings2.py
@@ -1,20 +1,35 @@
 parrot = "Norwegian Blue"
 
-print('using indexs.... def has to be an easier way to do this')
+# print('using indexs.... def has to be an easier way to do this')
 
-print(parrot)
-print(parrot[3])
-print(parrot[4])
-print()
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+# print(parrot)
+# print(parrot[3])
+# print(parrot[4])
+# print()
+# print(parrot[3])
+# print(parrot[6])
+# print(parrot[8])
 
-print('using negative indexs')
+# print('using negative indexs')
 
-print(parrot[-11])
-print(parrot[-1])
-print()
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+# print(parrot[-11])
+# print(parrot[-1])
+# print()
+# print(parrot[-11])
+# print(parrot[-8])
+# print(parrot[-6])
+
+# print(parrot[:6])
+# print(parrot[3:5])
+
+print(parrot[0:6:2]) 
+print(parrot[0:6:3])
+
+number = '9,223 372;376.854:777'
+
+seperators = number[1::4]
+
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
