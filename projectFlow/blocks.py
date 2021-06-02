09c76babbbca4dd8759237439b6fa9e2359eaee0
John Obertubbesing <oberthewhat@gmail.com>
2021-06-02 13:59:17 -0500

    added section 2

---
+++ projectFlow/blocks.py
@@ -0,0 +1,14 @@
+# for i in range(1, 13):
+#     print('no. {} squared is {} and cubed is {:4}'.format(i, i**2, i **3))
+# print("*" * 80)
+
+name = input("please enter your name: ")
+age = int(input("how old are  you, {0}? ".format(name)))
+
+print("{}, you are {}".format(name,age))
+
+if age >= 18:
+    print('you are old enough to vote')
+
+else:
+    print('you are not old enough to vote, please come back in {} years'.format(18 - age))
