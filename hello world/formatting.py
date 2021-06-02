16e7c0ad85f8f0850260611ecabb3a948a3faebe
John Obertubbesing <oberthewhat@gmail.com>
2021-06-02 13:31:53 -0500

    finished section 1

--- hello world/formatting.py
+++ hello world/formatting.py
@@ -1,2 +1,18 @@
 for i in range(1,13):
-    print("number: {0} squared is {1} and cubed is {2}".format(i, i **2, i**3))
+    print("number: {0:2} squared is {1:4} and cubed is {2:4}".format(i, i **2, i**3))
+
+print()
+
+for i in range(1,13):
+    print("number: {0:2} squared is {1:<4} and cubed is {2:<4}".format(i, i **2, i**3))
+
+
+
+print()
+
+print("pie is approximately {0:12}".format(22/7))
+print("pie is approximately {0:12f}".format(22/7))
+print("pie is approximately {0:12.50f}".format(22/7))
+print("pie is approximately {0:52.50f}".format(22/7))
+print("pie is approximately {0:62.50f}".format(22/7))
+print("pie is approximately {0:72.50f}".format(22/7))
