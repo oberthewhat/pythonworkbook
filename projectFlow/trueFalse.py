1a4c22478dd4f83c53938758d0e1df5514ffb5a8
John Obertubbesing <oberthewhat@gmail.com>
2021-06-03 13:12:49 -0500

    working with true and false

---
+++ projectFlow/trueFalse.py
@@ -0,0 +1,23 @@
+# day = "Friday"
+# temp = 30
+# raining = False
+
+# if(day == "Saturday" and temp > 27) or not raining:
+#     print('go swimming')
+# else:
+#     print('learn python')
+
+
+##### truthy values
+
+if 0:
+    print('true')
+else:
+    print("false")
+
+name = input('please enter your name: ')
+
+if name:
+    print('hello, {}'.format(name))
+else:
+    print('are you the man with no name')
